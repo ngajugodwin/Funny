b960959f088435084abe9e5909a30275d5f512de
ngajugodwin <ngajugodwin@yahoo.com>
2019-10-06 02:50:52 -0700

    Init FrontEnd

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  template: './app.component.html'
+})
+export class AppComponent {
+  name = 'Angularfjnjeijfoejfoj';
+}
